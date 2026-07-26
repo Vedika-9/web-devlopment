75cdc9e6b0043fdf0ece9f2c019ea5ce8f84ab60
Vedika-9 <vedikasakharkar27@gmail.com>
2026-07-26 21:21:03 +0530

    Javascript

---
+++ day12 js/script.js
@@ -0,0 +1,3 @@
+// to connect javascript file to html file, we use script tag in html
+// example <script src ="..."></script> . and this sentence is always written in last line and there should be no html code after this line.  
+console.log("hello from external Js")
